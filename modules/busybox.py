6ffd12e37bee91d77812b6564badde07730c3b19
r1k0 <erick@openchill.org>
2010-05-27 22:25:10 +0000

    update busybox output color

--- modules/busybox.py
+++ modules/busybox.py
@@ -1,10 +1,6 @@
 import os
 import sys
-color = os.getenv("GENKI_STD_COLOR")
-if color == '0':
-	from portage.output import *
-else:
-	from nocolor import green, turquoise, white, red, yellow
+from stdout import green, turquoise, white, red, yellow
 import utils
 
 def download(bb_version, quiet):
