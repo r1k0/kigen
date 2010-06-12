48dd269daa97428ebc4600944d9d49cf9886ff1c
r1k0 <erick@openchill.org>
2010-06-12 14:28:13 +0000

    fixed call to fail

--- modules/kernel.py
+++ modules/kernel.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from stdout import white, green, turquoise
+from stdout import white, green, turquoise, red, yellow
 import utils
 
 class kernel:
@@ -37,24 +37,19 @@ class kernel:
     
         if self.mrproper is True:
             ret = mrproper(self.kerneldir, self.KV, self.master_config, self.arch, self.quiet)
-            if ret is not zero:
-                raise error.fail('kernel.mrproper()')
+            if ret is not zero: self.fail('mrproper')
         if self.allyesconfig is True:
             ret = allyesconfig(self.kerneldir, self.KV, self.master_config, self.arch, self.quiet)
-            if ret is not zero:
-                raise error.fail('kernel.allyesconfig()')
+            if ret is not zero: self.fail('allyesconfig')
         elif self.allnoconfig is True:
             ret = allnoconfig(self.kerneldir, self.KV, self.master_config, self.arch, self.quiet)
-            if ret is not zero:
-                raise error.fail('kernel.allnoconfig()')
+            if ret is not zero: self.fail('allnoconfig')
         if self.menuconfig is True or self.kconf is not '':
             ret = menuconfig(self.kerneldir, self.KV, self.master_config, self.arch, self.quiet)
-            if ret is not zero:
-                raise error.fail('kernel.menuconfig()')
+            if ret is not zero: self.fail('menuconfig')
         if self.oldconfig is True:
             ret = oldconfig(self.kerneldir, self.KV, self.master_config, self.arch, self.quiet)
-            if ret is not zero:
-                raise error.fail('kernel.oldconfig()')
+            if ret is not zero: self.fail('oldconfig')
     
         # check for kernel .config
         if os.path.isfile(self.kerneldir+'/.config') is not True:
@@ -62,25 +57,21 @@ class kernel:
     
         # prepare
         ret = prepare(self.kerneldir, self.KV, self.master_config, self.arch, self.quiet)
-        if ret is not zero:
-            raise error.fail('kernel.prepare()')
+        if ret is not zero: self.fail('prepare')
     
         # bzImage
         ret = bzImage(self.kerneldir, self.KV, self.master_config, self.arch, self.quiet)
-        if ret is not zero:
-            raise error.fail('kernel.bzImage()')
+        if ret is not zero: self.fail('bzImage')
     
         # modules
         # if --allnoconfig is passed, then modules are disabled
         if self.allnoconfig is not True:
             ret = modules(self.kerneldir, self.KV, self.master_config, self.arch, self.quiet)
-            if ret is not zero:
-                raise error.fail('kernel.modules()')
+            if ret is not zero: self.fail('modules')
             if self.nomodinstall is False:
                 # modules_install
                 ret = modules_install(self.kerneldir, self.KV, self.master_config, self.arch, self.quiet, self.fakeroot)
-                if ret is not zero:
-                    raise error.fail('kernel.modules_install()')
+                if ret is not zero: self.fail('modules_install')
         # save kernel config
         if self.nosaveconfig is False:
             if os.path.isdir('/etc/kernels/'):
@@ -88,6 +79,15 @@ class kernel:
             else:
                 utils.sprocessor('mkdir /etc/kernels', self.verbose)
                 utils.sprocessor('cp %s %s' % (self.kerneldir+'/.config', '/etc/kernels/kernel-config-kigen-'+self.arch+'-'+self.KV), self.verbose)
+
+    def fail(self, step):
+        """
+        @arg step   string
+
+        @return     exit
+        """
+        print red('error')+': kernel.'+step+'() failed'
+        sys.exit(2)
 
 def copy_dotconfig(kernel_config, kerneldir, quiet):
     """
