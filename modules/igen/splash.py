19c3ffbf37489bc81a9d6d9f7af8ef92247635d2
r1k0 <erick@openchill.org>
2010-06-24 21:32:54 +0000

    add TODO for splash

--- modules/igen/splash.py
+++ modules/igen/splash.py
@@ -22,6 +22,7 @@ class splash:
         self.sinitrd        = sinitrd
         self.temp           = temp
         self.verbose        = verbose
+        self.splashtmp      = self.temp['work']+'/initramfs-splash-temp/'
 
     def build(self):
         """
@@ -29,7 +30,24 @@ class splash:
 
         @return     bool
         """
-        pass
+        utils.sprocessor('mkdir -p ' + self.splashtmp + '/dev', self.verbose)
+        utils.sprocessor('mkdir -p ' + self.splashtmp + '/etc', self.verbose)
+        utils.sprocessor('mkdir -p ' + self.splashtmp + '/root', self.verbose)
+        utils.sprocessor('mkdir -p ' + self.splashtmp + '/sbin', self.verbose)
+
+# create /lib or /lib64 depends on system host or why not just both?
+# create /lib/splash/proc /lib/splash/sys
+# create /dev/console /dev/tty0 /dev/null nods
+# create /dev/fb /dev/misc /dev/vc dir
+# copy initrd.splash to $/etc
+# create /etc/splash
+# copy theme dir from /etc/splash/theme to $/etc/splash according to sres too
+
+# compile fbcondecor_helper and copy it to /sbin 
+#  libpng jpeg zlib freetype
+#  mimic splashutils ebuild
+# make a symlink from /sbin/fbcondecor_helper to /sbin/splash_helper
+
 
     def fail(self, step):
         """
