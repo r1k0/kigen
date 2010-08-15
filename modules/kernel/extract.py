937b103f9a60f226b593ea6cab8a61fd002ff9f9
r1k0 <erick@openchill.org>
2010-08-16 01:12:46 +0200

    kigen k --getdotconfig=binary should work if IKCONFIG is set

--- modules/kernel/extract.py
+++ modules/kernel/extract.py
@@ -1,12 +1,18 @@
 import os
 import sys
+from utils.shell import *
+from stdout import white, green, turquoise, yellow, red
 
 def getdotconfig(binary, kerneldir, verbose):
-    if os.path.isfile(kerneldir+'/scripts/extract-ikconfig'):
+
+    print green(' * ')+turquoise('kernel.extract.getdotconfig ')+'from '+binary+' to /var/tmp/kigen/dotconfig'
+
+    if not os.path.isfile(kerneldir+'/scripts/extract-ikconfig'):
         print 'err: kernel sources not found'
+        sys.exit(2)
 
     if os.path.isfile('/var/tmp/kigen/dotconfig'):
         from time import strftime
         os.system('mv %s %s-%s ' % ('/var/tmp/kigen/dotconfig', '/var/tmp/kigen/dotconfig', strftime("%Y-%m-%d-%H-%M-%S")))
 
-    os.system('%s %s > /var/tmp/kigen/dotconfig')
+    os.system('%s %s > /var/tmp/kigen/dotconfig &>/dev/null' % (kerneldir+'/scripts/extract-ikconfig', binary))
