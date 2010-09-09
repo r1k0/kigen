08e6e50bb1826a958a499183c1646ebd02dad459
r1k0 <erick@openchill.org>
2010-09-09 12:02:51 +0200

    bug --into= fixed

--- modules/initramfs/compress.py
+++ modules/initramfs/compress.py
@@ -12,9 +12,9 @@ def initramfs(temproot, compress, into, verbose):
     """
     print green(' * ')+turquoise('initramfs.compress.initramfs ')+'from '+compress+' into '+into
 
-    if os.path.isdir(os.path.dirname(into)):
+    if os.path.isfile(into):
         from time import strftime
-        os.system('mv %s %s-%s ' % (os.path.dirname(into), os.path.dirname(into), strftime("%Y-%m-%d-%H-%M-%S")))
+        os.system('mv %s %s-%s ' % (into, into, strftime("%Y-%m-%d-%H-%M-%S")))
 
     if not os.path.isdir(os.path.dirname(into)):
         os.makedirs(os.path.dirname(into))
@@ -23,3 +23,4 @@ def initramfs(temproot, compress, into, verbose):
     os.chdir(compress)
     process_pipe('find . -print | cpio --quiet -o -H newc --append -F %s/initramfs_data.cpio' % os.path.dirname(into), verbose) 
     process('gzip %s/initramfs_data.cpio' % os.path.dirname(into), verbose)
+    process('mv %s/initramfs_data.cpio.gz %s' % (os.path.dirname(into), into), verbose)
