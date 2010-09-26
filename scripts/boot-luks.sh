59ab842fed22f82e93754e2df79bbff5e50f1935
r1k0 <erick@openchill.org>
2010-09-26 19:51:04 +0200

    rm space

--- scripts/boot-luks.sh
+++ scripts/boot-luks.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-
 if [ "$1" = "-h" ] || [ "$1" = "" ]
 then
     echo "$0 <root device>"
     exit
 fi
-
 /sbin/cryptsetup luksOpen $1 root
 mkdir /newroot
 /sbin/ttyecho -n /dev/console exit
