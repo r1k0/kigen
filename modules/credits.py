13eee3f896afa85d704c2d7ab841715a43c00926
r1k0 <erick@openchill.org>
2010-12-10 16:58:23 +0100

    add ac1965 to credits too

--- modules/credits.py
+++ modules/credits.py
@@ -5,4 +5,4 @@ version     = "0.2.4"
 productname = os.path.basename(sys.argv[0])
 description = "a kernel|initramfs generator."
 author      = [ 'erick "r1k0" michau - <erick@openchill.org>', '']
-contributor = [ 'alexis "shaye" vachette - <alexis@unhu.fr>', '']
+contributor = [ 'alexis "shaye" vachette - <alexis@unhu.fr>', '"ac1965" <tjy1965@gmail.com>']
