ef9024409c771b7e6643c6f9e6c07fca58ef0df8
r1k0 <erick@openchill.org>
2011-01-14 18:19:10 +0100

    get dynamic keymap list

--- modules/initramfs/append.py
+++ modules/initramfs/append.py
@@ -1004,20 +1004,17 @@ class append:
 
         @return bool
         """
-# use this to display dynamic keymaps
-# it's not really necessary but it's cleaner
-# 
-#        import os
-#        f=os.popen("ls /var/tmp/kigen/work/initramfs-keymaps-temp/lib/keymaps/")
-#        for i in f.readlines():
-# add regex to filter off: if i contains integers don't display it
-#                 print(i)
-
-        logging.debug('>>> entering initramfs.append.keymaps be bg br-a br-l by cf croat cz de dk dvorak es et fi fr gr hu il is it jp la lt mk nl no pl pt ro ru se sg sk-y sk-z slovene trf trq ua uk us wangbe')
-        print(green(' * ') + turquoise('initramfs.append.keymaps ')+ 'be bg br-a br-l by cf croat cz de dk dvorak es et fi fr gr hu il is it jp la lt mk nl no pl pt ro ru se sg sk-y sk-z slovene trf trq ua uk us wangbe')
+        logging.debug('>>> entering initramfs.append.keymaps')
+        print(green(' * ') + turquoise('initramfs.append.keymaps '))
 
         process('mkdir -p %s' % self.temp['work']+'/initramfs-keymaps-temp/lib/keymaps', self.verbose)
         process('tar -zxf %s/defaults/keymaps.tar.gz -C %s/initramfs-keymaps-temp/lib/keymaps' % (self.libdir, self.temp['work']), self.verbose)
+
+        f = os.popen("ls %s/initramfs-keymaps-temp/lib/keymaps/"%self.temp['work'])
+        for i in f.readlines():
+            # filter out item with numbers
+            if re.match("^[a-z]", i):
+                print(green(' * ') + '... ' +i, end='')
 
         os.chdir(self.temp['work']+'/initramfs-keymaps-temp')
         return os.system(self.cpio())
