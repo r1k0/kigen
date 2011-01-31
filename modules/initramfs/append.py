892e3aa44312f2fd6c87b98e5d07b832d3cfcca6
r1k0 <erick@openchill.org>
2011-01-31 17:55:13 +0100

    bump GK linuxrc to 3.4.12.5

--- modules/initramfs/append.py
+++ modules/initramfs/append.py
@@ -115,7 +115,7 @@ class append:
         #       os.system('cp %s/arch/linuxrc %s/initramfs-base-temp/init' % (libdir, temp['work']))
         # elif arch is 'amd64':
         #       blablabla
-            print(green(' * ')+'... Gentoo linuxrc'+ white(' 3.4.12.1') + ' patched')
+            print(green(' * ')+'... Gentoo linuxrc'+ white(' 3.4.12.5') + ' patched')
             # this is Gentoo official linuxrc suite (see genkernel)
             process('cp %s/defaults/linuxrc %s/initramfs-base-temp/init' % (self.libdir, self.temp['work']), self.verbose)
             process('cp %s/defaults/initrd.scripts %s/initramfs-base-temp/etc/' % (self.libdir, self.temp['work']), self.verbose)
