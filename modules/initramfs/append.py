d344b850411bfb0f7b7e3ceae8eba95e561bb4a9
r1k0 <erick@openchill.org>
2011-06-10 22:32:55 +0200

    clean

--- modules/initramfs/append.py
+++ modules/initramfs/append.py
@@ -325,37 +325,37 @@ class append:
         @return: bool
         """
         logging.debug('>>> entering initramfs.append.bin_luks')
-        cryptsetup_bin  = '/bin/cryptsetup'
+#        cryptsetup_bin  = '/bin/cryptsetup'
         cryptsetup_sbin = '/sbin/cryptsetup'
 
         process('mkdir -p %s' % self.temp['work']+'/initramfs-bin-luks-temp/lib/luks', self.verbose)
         process('mkdir -p %s' % self.temp['work']+'/initramfs-bin-luks-temp/sbin', self.verbose)
 
-        if os.path.isfile(cryptsetup_bin) and isstatic(cryptsetup_bin, self.verbose):
-            # use from host
-            logging.debug('initramfs.append.bin_luks from %s' % white('host'))
-            print(green(' * ') + turquoise('initramfs.append.bin_luks ')+ cryptsetup_bin +' from ' + white('host'))
-            process('cp %s %s/initramfs-bin-luks-temp/sbin' % (cryptsetup_bin, self.temp['work']), self.verbose)
-            process('chmod +x %s/initramfs-bin-luks-temp/sbin/cryptsetup' % self.temp['work'], self.verbose)
-
-#            if not isstatic(cryptsetup_bin, self.verbose):
-#                luks_libs = listdynamiclibs(cryptsetup_bin, self.verbose)
+#        if os.path.isfile(cryptsetup_bin) and isstatic(cryptsetup_bin, self.verbose):
+#            # use from host
+#            logging.debug('initramfs.append.bin_luks from %s' % white('host'))
+#            print(green(' * ') + turquoise('initramfs.append.bin_luks ')+ cryptsetup_bin +' from ' + white('host'))
+#            process('cp %s %s/initramfs-bin-luks-temp/sbin' % (cryptsetup_bin, self.temp['work']), self.verbose)
+#            process('chmod +x %s/initramfs-bin-luks-temp/sbin/cryptsetup' % self.temp['work'], self.verbose)
 #
-#                process('mkdir -p %s' % self.temp['work']+'/initramfs-luks-temp/lib', self.verbose)
-#                print green(' * ') + '... ' + yellow('warning')+': '+cryptsetup_bin+' is dynamically linked, copying detected libraries'
-#                for i in luks_libs:
-#                    print green(' * ') + '... ' + i
-#                    process('cp %s %s' % (i, self.temp['work']+'/initramfs-luks-temp/lib'), self.verbose)
-#            else:
-#                logging.debug(cryptsetup_bin+' is statically linked nothing to do')
-
-#        elif os.path.isfile(cryptsetup_sbin) and self.hostbin is True and isstatic(cryptsetup_sbin, self.verbose):
-        elif os.path.isfile(cryptsetup_sbin) and isstatic(cryptsetup_sbin, self.verbose):
+##            if not isstatic(cryptsetup_bin, self.verbose):
+##                luks_libs = listdynamiclibs(cryptsetup_bin, self.verbose)
+##
+##                process('mkdir -p %s' % self.temp['work']+'/initramfs-luks-temp/lib', self.verbose)
+##                print green(' * ') + '... ' + yellow('warning')+': '+cryptsetup_bin+' is dynamically linked, copying detected libraries'
+##                for i in luks_libs:
+##                    print green(' * ') + '... ' + i
+##                    process('cp %s %s' % (i, self.temp['work']+'/initramfs-luks-temp/lib'), self.verbose)
+##            else:
+##                logging.debug(cryptsetup_bin+' is statically linked nothing to do')
+#
+##        elif os.path.isfile(cryptsetup_sbin) and self.hostbin is True and isstatic(cryptsetup_sbin, self.verbose):
+#        elif os.path.isfile(cryptsetup_sbin) and isstatic(cryptsetup_sbin, self.verbose):
             # use from host
-            logging.debug('initramfs.append.bin_luks from %s' % white('host'))
-            print(green(' * ') + turquoise('initramfs.append.bin_luks ')+ cryptsetup_sbin +' from ' + white('host'))
-            process('cp %s %s/initramfs-bin-luks-temp/sbin' % (cryptsetup_sbin, self.temp['work']), self.verbose)
-            process('chmod +x %s/initramfs-bin-luks-temp/sbin/cryptsetup' % self.temp['work'], self.verbose)
+        logging.debug('initramfs.append.bin_luks from %s' % white('host'))
+        print(green(' * ') + turquoise('initramfs.append.bin_luks ')+ cryptsetup_sbin +' from ' + white('host'))
+        process('cp %s %s/initramfs-bin-luks-temp/sbin' % (cryptsetup_sbin, self.temp['work']), self.verbose)
+        process('chmod +x %s/initramfs-bin-luks-temp/sbin/cryptsetup' % self.temp['work'], self.verbose)
 
 #            if not isstatic(cryptsetup_sbin, self.verbose):
 #                luks_libs = listdynamiclibs(cryptsetup_sbin, self.verbose)
