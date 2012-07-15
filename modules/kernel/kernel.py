e1291c35fe6397177537b8df7b28ca4b8538217b
r1k0 <erick@openchill.org>
2012-07-15 23:53:59 +0000

    issue6

--- modules/kernel/kernel.py
+++ modules/kernel/kernel.py
@@ -384,11 +384,9 @@ class kernel:
         """
         print(green(' * ') + turquoise('kernel.oldconfig '))
         self.chgdir(self.kerneldir)
-        command = self.build_command('oldconfig', self.quiet)
-# NOFIX: should work
-# FIXME: this breaks if Kicktoo calls KIGen to build a kernel error is due to a broken pipe
-#        command = 'yes "" 2>/dev/null | make oldconfig'
-#       then try command = 'yes "" &>/dev/null | make oldconfig'
+        command = 'yes "" &>/dev/null | ' + self.build_command('oldconfig', '')
+#        command = 'yes "" &>/dev/null | ' + self.build_command('oldconfig', self.quiet)
+# FIXME command = command + '; test ${PIPESTATUS[1]} -eq 0'
         if self.quiet is '':
             print(command)
 
