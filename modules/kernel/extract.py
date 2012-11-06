ca9c4704d9a04da558d7ecbd8f6cebdcd6a89961
r1k0 <erick@openchill.org>
2012-11-06 18:54:58 +0000

    check for binayr file

--- modules/kernel/extract.py
+++ modules/kernel/extract.py
@@ -7,6 +7,10 @@ def getdotconfig(binary, kerneldir, libdir, verbose):
 
     print(green(' * ')+turquoise('tool.extract.kernel.getdotconfig ')+'from '+binary+' to /var/tmp/kigen/dotconfig')
 
+    if not os.path.isfile(binary):
+        print(red('error')+': '+binary+' is not a file!')
+        sys.exit(2)
+
     if os.path.isfile('/var/tmp/kigen/dotconfig'):
         from time import strftime
         os.system('mv %s %s-%s ' % ('/var/tmp/kigen/dotconfig', '/var/tmp/kigen/dotconfig', strftime("%Y-%m-%d-%H-%M-%S")))
